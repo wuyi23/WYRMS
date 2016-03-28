8762e23c2d2823bf8cc4ee671a253acfa8ce2697
wy <375368093@qq.com>
2016-03-28 13:53:23 +0800

    项目中用到的Nuget程序包上传
    项目中用到的Nuget程序包上传

--- src/WY.RMS.Component.Data/Migrations/Configuration.cs
+++ src/WY.RMS.Component.Data/Migrations/Configuration.cs
@@ -31,6 +31,7 @@ namespace WY.RMS.Component.Data.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            #region 模块管理
             List<Module> modules = new List<Module>
             {
                 new Module { Id = 1, ParentId = null, Name = "授权管理", Code = 200,LinkUrl="#",  Description = null, IsMenu = true, Enabled = true,  UpdateDate = DateTime.Now},
@@ -45,10 +46,12 @@ namespace WY.RMS.Component.Data.Migrations
             DbSet<Module> moduleSet = context.Set<Module>();
             moduleSet.AddOrUpdate(t => new { t.Name }, modules.ToArray());
             context.SaveChanges();
+            #endregion
 
+            #region 权限管理
             List<Permission> permissions = new List<Permission>
             {
-             #region 角色管理
+             #region 角色
 		       new Permission{Id=1, Name="查询",Code=EnumPermissionCode.QueryRole.ToString(), 
                     Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[1]},
                new Permission{Id=2, Name="新增",Code=EnumPermissionCode.AddRole.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[1]},
@@ -57,7 +60,7 @@ namespace WY.RMS.Component.Data.Migrations
                new Permission{Id=5, Name="授权",Code=EnumPermissionCode.AuthorizeRole.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[1]}, 
              #endregion
 
-             #region 用户管理
+             #region 用户
 		       new Permission{Id=6, Name="查询",Code=EnumPermissionCode.QueryUser.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[2]},
                new Permission{Id=7, Name="新增",Code=EnumPermissionCode.AddUser.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[2]},
                new Permission{Id=8, Name="修改",Code=EnumPermissionCode.UpdateUser.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[2]},
@@ -66,26 +69,29 @@ namespace WY.RMS.Component.Data.Migrations
                new Permission{Id=11, Name="设置用户组",Code=EnumPermissionCode.SetGroupUser.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[2]},
                new Permission{Id=12, Name="设置角色",Code=EnumPermissionCode.SetRolesUser.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[2]}, 
 	         #endregion
-               
-             #region 模块管理
+             
+             #region 模块
 		     new Permission{Id=13, Name="查询",Code=EnumPermissionCode.QueryModule.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[3]},
              new Permission{Id=14, Name="新增",Code=EnumPermissionCode.AddModule.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[3]},
              new Permission{Id=15, Name="修改",Code=EnumPermissionCode.UpdateModule.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[3]},
 	         #endregion
 
-             #region 权限管理
+             #region 权限
 		     new Permission{Id=16, Name="查询",Code=EnumPermissionCode.QueryPermission.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[4]},
              new Permission{Id=17, Name="新增",Code=EnumPermissionCode.AddPermission.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[4]},
              new Permission{Id=18, Name="修改",Code=EnumPermissionCode.UpdatePermission.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[4]},
 	         #endregion
 
-             #region 操作日志管理
+             #region 操作日志
 		     new Permission{Id=19, Name="查询",Code=EnumPermissionCode.QuerySystemLog.ToString(), Description="描述" ,Enabled=true,UpdateDate=DateTime.Now,module=modules[6]}
 	         #endregion
             };
             DbSet<Permission> permissionSet = context.Set<Permission>();
             permissionSet.AddOrUpdate(m => new { m.Id }, permissionSet.ToArray());
             context.SaveChanges();
+            #endregion
+
+            #region 角色管理
             List<Role> roles = new List<Role>
             {
                 new Role { Id=1,  RoleName = "superadmin", Description="超级管理员",Enabled=true,OrderSort=1,UpdateDate=DateTime.Now ,Permissions=permissions},
@@ -104,7 +110,9 @@ namespace WY.RMS.Component.Data.Migrations
             DbSet<Role> roleSet = context.Set<Role>();
             roleSet.AddOrUpdate(m => new { m.RoleName }, roles.ToArray());
             context.SaveChanges();
+            #endregion
 
+            #region 用户管理
             List<User> members = new List<User>
             {
                 new User { Id=1, UserName = "admin", Password = "000102030405060708090a0b0c0d0e0f", Email = "375368093@qq.com", TrueName = "管理员",Phone="18181818181",Address="广东广州市天河区科韵路XX街XX号XXX房X号" ,Enabled=true,Roles=new List<Role>{roles[1]} },
@@ -113,6 +121,18 @@ namespace WY.RMS.Component.Data.Migrations
             DbSet<User> memberSet = context.Set<User>();
             memberSet.AddOrUpdate(m => new { m.UserName }, members.ToArray());
             context.SaveChanges();
+            #endregion
+
+            #region 用户管理
+            List<UserGroup> userGroups = new List<UserGroup>
+            {
+                new UserGroup { Id=1, GroupName = "开发组",Description = "开发人员组",Enabled=true,Roles=new List<Role>{roles[1]} ,Users = new List<User>(){members[0]}},
+                new UserGroup { Id=2, GroupName = "项目经理组", Description = "项目经理组",Enabled=true,Roles=new List<Role>{roles[1]}, Users = new List<User>(){members[1]}}
+            };
+            DbSet<UserGroup> userGroupsSet = context.Set<UserGroup>();
+            userGroupsSet.AddOrUpdate(m => new { m.GroupName }, userGroups.ToArray());
+            context.SaveChanges();
+            #endregion
         }
     }
 }
